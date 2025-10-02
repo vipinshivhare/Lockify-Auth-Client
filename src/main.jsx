31d2d506d3868041900ba5d76d3d09b3d4692f1f
vipinshivhare <vipinshivhare804@gmail.com>
2025-10-03 00:21:35 +0530

    initial

--- src/main.jsx
+++ src/main.jsx
@@ -12,13 +12,12 @@ import "react-toastify/dist/ReactToastify.css";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-<BrowserRouter>
-    <AppContextProvider>
-        <App />
-    </AppContextProvider>
-
-</BrowserRouter>
-
-
-)
+    <StrictMode>
+      <BrowserRouter>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </BrowserRouter>
+    </StrictMode>
+  );
+  
